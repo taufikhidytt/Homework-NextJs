2a25b80010228739c7afbe2b6aa1040a2d5ffcd5
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-04-28 17:41:10 +0700

    update final homework

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
-export default function Home() {
+import prisma from "@/lib/prisma";
+import TodoList from "@/components/TodoList";
+
+// Server Component
+const getData = async () => {
+  try {
+    const data = await prisma.todo.findMany();
+    
+    return data;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+//   Server Component ==> Paling atas
+//        ||
+//        ||
+//        \/
+//   Client Component
+
+export default async function Home() {
+  const data = await getData();
   return (
     <div>
-      Test
-      pause 10 menit
+      <TodoList todos={data} />
     </div>
   );
 }
---
+++ src/fetching/todos.js
@@ -0,0 +1,51 @@
+import BASE_URL from "@/lib/baseUrl";
+
+export const deleteTodo = async (id) => {
+    try {
+
+        const response = await fetch(`${BASE_URL}/todos/${id}`, {
+            method: "DELETE"
+        })
+
+        return response;
+    } catch (err) {
+        throw new Error({
+            message: err.response.message
+        })
+    }
+}
+
+export const createTodo = async (params) => {
+
+    try {
+
+        const response = await fetch(`${BASE_URL}/todos`, {
+            method: "POST",
+            body: JSON.stringify(params)
+        })
+
+        return response
+    } catch (err) {
+        throw new Error({
+            message: err.response.message
+        })
+    }
+}
+
+export const uploadImage = async (params) => {
+
+    try {
+        const response = await fetch(`${BASE_URL}/todos/uploads`, {
+            method: "POST",
+            body: params
+        })
+
+        const data = await response.json();
+
+        return data
+    } catch (err) {
+        throw new Error({
+            message: err.response.message || "Internal Server Error"
+        })
+    }
+}
